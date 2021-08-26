3e946f9cc81b00fcbe89bdf3b0e7838f506ba844
mig <thefiregod2511@gmail.com>
2021-08-26 21:23:19 +1000

    Sorting Method
    No Idea if this works :wink:

--- src/util/functions.ts
+++ src/util/functions.ts
@@ -1,4 +1,30 @@
+import { TagTypes } from './constants';
+import COMPUTERS from './computers';
+
 // tag input is list of tags
-function findMatch(tags) {
-	// Check if array of string, if not return null
+function findMatch(tags: TagTypes[]) {
+	// Define variable to store number of Tag matches
+	let matches: {
+		place: number;
+		matches: number;
+	}[];
+	for (let computer in COMPUTERS) {
+		// computer represents its key within COMPUTERS
+		let place = parseInt(computer); // Initially a string, convert to integer to use as key elsewhere
+		for (let tag of COMPUTERS[place].tags) { // Loops through each tag of each computer
+			if (tags.includes(tag))
+				matches[place] = {
+					place,
+					matches: matches[place].matches++,
+				};
+		}
+	}
+	// Sort matches in descending order
+	const sortedMatches = matches.sort((a, b) => {
+		if (a.matches < b.matches) return 1; // Sort b before a as b is greater
+		if (a.matches > b.matches) return -1; // Sort a before b as a is greater
+		return 0; // Otherwise must be equal
+	});
+
+	return COMPUTERS[sortedMatches[0].place];
 }
