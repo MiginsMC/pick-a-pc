1150698a4c5a7e6e6bbfcf7b042585bf95ed181c
mig <thefiregod2511@gmail.com>
2021-08-27 12:23:02 +1000

    Update Quiz.tsx

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,7 +1,17 @@
+import { useState } from 'react';
+import COMPUTERS from '../../util/computers';
+
 function Quiz() {
+	const [place, setPlace] = useState(0);
+
+	function handleSubmit(event: React.FormEvent) {}
+
 	return (
 		<div>
 			Quiz bro <br />
+			<form name="pickAPC" onSubmit={handleSubmit}>
+				{}
+			</form>
 			<button type="button">Bad ram</button> OR{' '}
 			<button type="button">Good ram</button>
 		</div>
