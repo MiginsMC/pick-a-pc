baf7ab6f6c1e513b6d226ca9f31c130e6df5fbf2
mig <thefiregod2511@gmail.com>
2021-08-30 12:25:06 +1000

    Quiz progress

--- src/pages/quiz/Quiz.tsx
+++ src/pages/quiz/Quiz.tsx
@@ -1,17 +1,29 @@
-import React from 'react';
-import { useState } from 'react';
-import COMPUTERS from '../../util/computers';
+import React, { useState, FormEvent } from 'react';
+import QUESTIONS from '../../util/questions';
 
 function Quiz() {
 	const [place, setPlace] = useState(0);
+	const [startedQuiz, setStartedQuiz] = useState(false);
+	const question = QUESTIONS[place].question;
+	const help = QUESTIONS[place].help || undefined;
+	const options = QUESTIONS[place].options;
 
-	function handleSubmit(event: React.FormEvent) {}
+	const optionsFormatted = options.map(e => (
+		<p>
+			<button type="submit" name={e.option}>
+				{e.option}
+			</button>
+		</p>
+	));
+
+	function handleSubmit(event: FormEvent) {}
 
 	return (
 		<div>
 			Quiz bro <br />
+			<p>{question}</p>
 			<form name="pickAPC" onSubmit={handleSubmit}>
-				{}
+				{options}
 			</form>
 			<button type="button">Bad ram</button> OR{' '}
 			<button type="button">Good ram</button>
